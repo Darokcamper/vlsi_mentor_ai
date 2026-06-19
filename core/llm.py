f0ecbb62e0c8944eb7c89b8f876d882b13fe7427
Darokcamper <hazarh833@gmail.com>
2026-06-19 16:17:17 +0530

    Update llm.py

--- core/llm.py
+++ core/llm.py
@@ -1,9 +1,15 @@
-from langchain_groq import ChatGroq
-from dotenv import load_dotenv
+import os
+import streamlit as st
 
-load_dotenv()
+from langchain_groq import ChatGroq
+
+api_key = os.getenv("GROQ_API_KEY")
+
+if not api_key:
+    api_key = st.secrets["GROQ_API_KEY"]
 
 llm = ChatGroq(
     model="llama-3.3-70b-versatile",
+    api_key=api_key,
     temperature=0.8
 )
